d711fbc7728e84554dca1594f929cd5f17969052
Rename22 <stiven.chillagana7807@utc.edu.ec>
2025-06-18 21:44:53 -0500

    Fix editing schema and primary key

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -42,6 +42,7 @@ class Database extends Config
         'strictOn'     => false,
         'failover'     => [],
         'port'         => 5432,
+        'schema'       => 'laboratorios',
         'numberNative' => false,
         'foundRows'    => false,
         'dateFormat'   => [
--- app/Models/ReservaModel.php
+++ app/Models/ReservaModel.php
@@ -6,16 +6,39 @@ use CodeIgniter\Model;
 
 class ReservaModel extends Model
 {
-    protected $table = 'laboratorios.reserva';
-    protected $primaryKey = 'id_reserva';
+    protected $table      = 'laboratorios.reserva';
+    protected $primaryKey = 'id_res';
     protected $allowedFields = [
-        'fk_id_lab',
+        'fk_id_tipres',
         'fk_id_doc',
-        'fecha_reserva',
-        'hora_inicio_reserva',
-        'hora_fin_reserva',
-        'estado_reserva',
-        'motivo_reserva',
+        'fk_id_lab',
+        'fk_id_area',
+        'fk_id_guia',
+        'tema_res',
+        'comentario_res',
+        'estado_res',
+        'fecha_hora_res',
+        'duracion_res',
+        'numero_participantes_res',
+        'descripcion_participantes_res',
+        'materiales_res',
+        'fecha_creacion_res',
+        'fecha_actualizacion_res',
+        'usuario_creacion_res',
+        'usuario_actualizacion_res',
+        'fecha_hora_fin_res',
+        'observaciones_finales_res',
+        'asistencia_res',
+        'guia_adjunta_res',
+        'curso_res',
+        'materia_res',
+        'fk_id_car',
+        'paralelo_res',
+        'tipo_texto_res',
+        'fk_id_usu',
+        'software_res',
+        'tipo_res',
+        'pedidodocente_res',
     ];
 
     public function getAllReservas()
